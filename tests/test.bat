197ca5d23b68f470325874954240c5b5a8006902
Bluscream <admin@timo.de.vc>
2020-04-16 10:40:58 +0200

    UPDATE 04/16/20 10:40:57

---
+++ tests/test.bat
@@ -0,0 +1,22 @@
+@echo off
+set err_level=0
+
+rem http://ss64.com/nt/start.html
+rem http://stackoverflow.com/questions/138497/iterate-all-files-in-a-directory-using-a-for-loop for file loops
+rem "C:/Program Files/AutoHotkey/AutoHotkeyU32.exe" /ErrorStdOut "testMain.ahk" 2>&1 |more
+
+rem Loop over all ahk files in tests directory
+for /r %%i in (*.ahk) do (
+	start "testing" /B /wait "C:\Program Files\AutoHotkey\AutoHotkeyU32.exe" /ErrorStdOut %%~nxi > testoutput.txt 2>&1
+	echo ** Running %%~nxi **
+	if errorlevel 1 (
+		echo *** Test file %%~nxi failed ***
+		set err_level=1
+	)
+	type testoutput.txt
+	echo.
+)
+
+
+rem EXIT SCRIPT
+exit /b %err_level%
